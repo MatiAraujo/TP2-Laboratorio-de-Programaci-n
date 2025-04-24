a0e62d8525b248e38f1385f96d48c5af32f43e09
MatiAraujo <mati.araujo1998@gmail.com>
2025-04-24 17:44:30 -0300

    Ejercicio 9 al 11

--- Articulo.py
+++ Articulo.py
@@ -5,12 +5,88 @@ class Articulo:
         self.texto = texto.strip()
 
     def to_html(self):
+        if len(self.texto) > 300:
+            texto_recortado = self.texto[:300] + '...'
+        else:
+            texto_recortado = self.texto
         return f"""
         <div class="articulo">
             <h2>{self.titulo}</h2>
-            <p>{self.texto}</p>
+            <p>{texto_recortado}</p>
         </div>
         """
     
     def contiene_palabra(self, palabra):
         return palabra.lower() in self.texto.lower()
+    
+    def generar_html_individual(self):
+        nombre_archivo = self.titulo.lower().replace(" ", "_").replace("á", "a").replace("é", "e").replace("í", "i").replace("ó", "o").replace("ú", "u") + ".html"
+
+        contenido = f"""<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>{self.titulo}</title>
+    <style>
+        body {{
+                font-family: Arial, sans-serif;
+                background-color: #f9f9f9;
+                background-image: url(fondo.jpg);
+                background-size: cover;
+                background-repeat: no-repeat;
+                background-attachment: fixed;
+                color: #333;
+                margin: 0;
+                padding: 20px;
+            }}
+            .articulo {{
+                background-color: #00a5e5;
+                color: white;
+                border-radius: 8px;
+                padding: 5px 5px;
+                margin-top: 5px;
+                font-size: 0.9em;
+                box-shadow: 0 2px 4px rgba(0,0,0,0.1);
+            }}
+             h1 {{
+                color: #ffffff;
+            }}
+            h4 {{
+                color: #d9f3ff;
+                font-weight: normal;
+                font-size: 0.95em;
+            }}
+            p {{
+                color: #f0f8ff;
+                margin: 0;
+                line-height: 1.4;
+            }}
+        a {{
+            display: inline-block;
+            margin-bottom: 20px;
+            color: #007BFF;
+        }}
+        .articulo_ind{{
+                margin-top: 30px;
+                background-color: rgba(247,120,56,1);
+                border-radius: 8px;
+                padding: 16px 12px;
+        }}
+        
+        
+    </style>
+</head>
+<body>
+    <a href="index.html">← Volver al inicio</a>
+    <div class="articulo_ind">
+        <h1>{self.titulo}</h1>
+        <h4>Por {self.autor}</h4>
+        <p class="articulo">{self.texto}</p>
+    </div>
+</body>
+</html>"""
+
+        with open(nombre_archivo, "w", encoding="utf-8") as f:
+            f.write(contenido)
+
+        return nombre_archivo  
--- ParserHtml.py
+++ ParserHtml.py
@@ -28,138 +28,194 @@ class ParserHtml:
         for articulo in self.articulos:
             articulos_por_autor[articulo.autor].append(articulo)
 
+        #crear indices
         indice_autores = "<div class='indice'><h2>Autores</h2><ul>\n"
         for autor in articulos_por_autor:
             id_autor = autor.lower().replace(" ", "-")
-            indice_autores += f'<li><a href="#{id_autor}">{autor}</a></li>\n'
+            indice_autores += f'<li><a href="autor-{id_autor}.html">{autor}</a></li>\n'
         indice_autores += "</ul></div>\n"
 
-        contenido_articulos = ""
+       
 
         for autor, articulos in articulos_por_autor.items():
             id_autor = autor.lower().replace(" ", "-")
-            contenido_articulos += f"""
+            
+            # Generar el HTML para cada autor
+            contenido_autor = f"""
             <div class="autor" id="{id_autor}">
-                <h3>{autor}</h3>
+            <h1>Artículos de {autor}</h1>    
             """
-
             for articulo in articulos:
-                contenido_articulos += articulo.to_html()
-            contenido_articulos += "</div>\n"
+                nombre_archivo = articulo.generar_html_individual()
+                contenido_autor += f"""
+                <div class="articulo">
+                    <h2><a href="{nombre_archivo}">{articulo.titulo}</a></h2>
+                </div>
+                """
+            contenido_autor += "</div>\n"
+            
+            with open(f"autor-{id_autor}.html", 'w', encoding='utf-8') as f:
+                f.write(f"""<!DOCTYPE html>
+    <html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>Artículos de {autor}</title>
+        <style>
+            body {{
+                font-family: Arial, sans-serif;
+                background-color: #f9f9f9;
+                background-image: url(fondo.jpg);
+                background-size: cover;
+                background-repeat: no-repeat;
+                background-attachment: fixed;
+                color: #333;
+                margin: 0;
+                padding: 20px;
+            }}
+            a {{
+                display: inline-block;
+                color: white;
+            }}
 
+            .articulo {{
+                background-color: #00a5e5;
+                color: white;
+                border-radius: 8px;
+                padding: 5px;
+                margin-top: 5px;
+                font-size: 0.9em;
+                box-shadow: 0 2px 4px rgba(0,0,0,0.1);
+            }}
+            .articulo h2 {{
+                color: #ffffff;
+            }}
+            .articulo h4 {{
+                color: #d9f3ff;
+                font-weight: normal;
+                font-size: 0.95em;
+            }}
+            .articulo p {{
+                color: #f0f8ff;
+                margin: 0;
+                line-height: 1.4;
+            }}
+            .autor h3 {{
+                margin-top: 0px;
+                margin-bottom: 5px;
+            }}
+            .autor {{
+                margin-top: 30px;
+                background-color: rgba(247,120,56,1);
+                border-radius: 8px;
+                padding: 16px 12px;
+            }}
+
+        </style>
+    </head>
+    <body>
+        <a href="index.html">← Volver al inicio</a>
+        
+        {contenido_autor}
+    </body>
+    </html>
+    """)
+
+        #página principal
+        # no importa style articulo y autor
         html_completo = f"""<!DOCTYPE html>
-<html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <title>Noticias</title>
+    <html lang="es">
+    <head>
+        <meta charset="UTF-8">
+        <title>Noticias</title>
+        <style>
+            body {{
+                font-family: Arial, sans-serif;
+                background-color: #f9f9f9;
+                background-image: url(fondo.jpg);
+                background-size: cover;
+                background-repeat: no-repeat;
+                background-attachment: fixed;
+                color: #333;
+                margin: 0;
+                padding: 20px;
+            }}
+            header, footer {{
+                background-color: rgba(247,120,56,1);
+                color: white;
+                padding: 15px;
+                text-align: center;
+            }}
+            .articulo {{
+                background-color: #00a5e5;
+                color: white;
+                border-radius: 8px;
+                padding: 5px;
+                margin-top: 5px;
+                font-size: 0.9em;
+                box-shadow: 0 2px 4px rgba(0,0,0,0.1);
+            }}
+            footer{{
+                margin-top: 30px;
+            }}
+            
+            .articulo h2 {{
+                color: #ffffff;
+            }}
+            .articulo h4 {{
+                color: #d9f3ff;
+                font-weight: normal;
+                font-size: 0.95em;
+            }}
+            .articulo p {{
+                color: #f0f8ff;
+                margin: 0;
+                line-height: 1.4;
+            }}
+            .autor h3 {{
+                margin-top: 0px;
+                margin-bottom: 5px;
+            }}
+            .autor {{
+                margin-top: 30px;
+                background-color: rgba(247,120,56,1);
+                border-radius: 8px;
+                padding: 16px 12px;
+            }}
+            a {{
+                color: #007BFF;
+                text-decoration: none;
+            }}
+            a:visited {{
+                color: #007BFF;
+            }}
+            a:hover {{
+                text-decoration: underline;
+            }}
+            a:active {{
+                color: #0056b3;
+            }}
+        </style>
+    </head>
+    <body>
+        <header><h1>Noticias del día</h1></header>
+        {indice_autores}
+    </body>
+    <footer><p>Fecha de creación: {fecha} </p></footer>
+    </html>
+    """
 
-    <style>
-        body {{
-            font-family: Arial, sans-serif;
-            background-color: #f9f9f9;
-            background-image: url(fondo.jpg);
-            background-size: cover;
-            background-repeat: no-repeat;
-            background-attachment: fixed;
-            color: #333;
-            margin: 0;
-            padding: 20px;
-        }}
-
-        header, footer {{
-            background-color: rgba(247,120,56,1);
-            color: white;
-            padding: 15px;
-            text-align: center;
-        }}
-
-        .articulo {{
-            background-color: #00a5e5;
-            color: white;
-            border-radius: 8px;
-            padding: 5px;
-            margin-top: 5px;
-            font-size: 0.9em;
-            box-shadow: 0 2px 4px rgba(0,0,0,0.1);
-        }}
-
-        footer{{
-            margin-top: 30px;
-        }}
         
-        .articulo h2 {{
-            color: #ffffff;
-        }}
-
-        .articulo h4 {{
-            color: #d9f3ff;
-            font-weight: normal;
-            font-size: 0.95em;
-        }}
-
-        .articulo p {{
-            color: #f0f8ff;
-            margin: 0;
-            line-height: 1.4;
-        }}
-
-        .autor h3 {{
-            margin-top: 0px;
-            margin-bottom: 5px;
-        }}
-        
-        .autor {{
-            margin-top: 30px;
-            background-color: rgba(247,120,56,1);
-            border-radius: 8px;
-            padding: 16px 12px;
-        }}
-
-        a {{
-        color: #007BFF; /* o el color que prefieras */
-        text-decoration: none;
-        }}
-
-        a:visited {{
-        color: #007BFF; /* mismo color que el estado normal */
-        }}
-
-        a:hover {{
-        text-decoration: underline;
-        }}
-
-        a:active {{
-        color: #0056b3;
-        }}
-
-    </style>
-</head>
-<body>
-    <header><h1>Noticias del día</h1></header>
-    {indice_autores}
-    {contenido_articulos}
-</body>
-<footer><p>Fecha de creación: {fecha} </p></footer>
-</html>
-"""
-
         with open(archivo_html, 'w', encoding='utf-8') as f:
             f.write(html_completo)
 
 articulos = [
     ("Pronóstico del tiempo", "pablo pérez", "Hoy estará parcialmente soleado, y pueden haber chubascos por la tarde."),
-    ("Nueva apertura cultural en la ciudad", "Laura Fernández", "El teatro municipal reabre sus puertas con una muestra gratuita para todo público."),
+    ("Nueva apertura cultural en la ciudad", "Carla Suárez", "El teatro municipal reabre sus puertas con una muestra gratuita para todo público."),
     ("Tecnología al alcance de todos", "Martín Díaz", "Una startup local desarrolló una app que traduce lenguaje de señas en tiempo real."),
     ("Suben los precios de los alimentos", "Carla Suárez", "El índice de inflación marcó un aumento del 5% en productos de la canasta básica."),
-    ("Deportes: el equipo local sigue invicto", "Ian Colombo", "Con un gol en el último minuto, el equipo se mantiene primero en la tabla."),
-    ("Educación en tiempos digitales", "Mariana López", "El 70% de las escuelas ya incorporaron plataformas virtuales como complemento."),
-    ("Recomendaciones para el cuidado del medio ambiente", "Camila Torres", "Separar los residuos y reducir el uso de plásticos puede marcar la diferencia."),
-    ("Festival gastronómico en el parque", "Valentina Godoy", "Más de 30 puestos ofrecerán comidas típicas durante todo el fin de semana."),
-    ("La música vuelve a los escenarios", "Javier Gómez", "Este viernes se realizará un recital gratuito en la plaza central con bandas locales."),
-    ("Consejos para una vida saludable", "Camila Torres", "Dormir bien, hidratarse y caminar 30 minutos al día mejoran la salud general.")
+    ("Deportes, el equipo local sigue invicto", "Ian Colombo", "Con un gol en el último minuto, el equipo se mantiene primero en la tabla."),
+    
 ]
 
 nuevo = ParserHtml(articulos)
 nuevo.generar_html()
-
-
